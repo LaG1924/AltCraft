c76b0be299496997144871b4087c1ceb3629feae
UIS <uis9936@gmail.com>
2020-07-31 23:56:03 +0300

    Added 2D vector class

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -139,5 +139,118 @@ struct AC_API Vector3 {
     }
 };
 
+template<class T>
+struct AC_API Vector2 {
+	T x, z;
+
+	Vector2(T X = 0, T Z = 0) : x(X), z(Z) {}
+
+	Vector2(const Vector2 &rhs) : x(rhs.x), z(rhs.z) {}
+
+	~Vector2() = default;
+
+	double GetLength() const { return std::sqrt(std::pow(x, 2) + std::pow(z, 2)); }
+
+	void swap(Vector2 &rhs) noexcept {
+		std::swap(x, rhs.x);
+		std::swap(z, rhs.z);
+	}
+
+	T dot(const Vector2 &rhs) const {
+		return x*rhs.x + z*rhs.z;
+	}
+
+	double cosBetween(const Vector2<T> &rhs) const {
+		return dot(rhs) / GetLength() / rhs.GetLength();
+	}
+
+	Vector2<double> normalize() {
+		auto length = GetLength();
+
+		return Vector2<double> (
+				x / length,
+				z / length
+		);
+	}
+
+	Vector2 &operator=(Vector2 rhs) noexcept {
+		rhs.swap(*this);
+		return *this;
+	}
+
+	Vector2 operator*(T rhs) const {
+		return Vector2<T>(
+				x * rhs,
+				z * rhs
+		);
+	}
+
+	Vector2 operator/(T rhs) const {
+		return Vector2<T>(
+				x / rhs,
+				z / rhs
+		);
+	}
+
+	Vector2 operator+(const Vector2 &rhs) const {
+		return Vector2<T>(
+				x + rhs.x,
+				z + rhs.z
+		);
+	}
+
+	Vector2 operator-(const Vector2 &rhs) const {
+		return Vector2<T>(
+				x - rhs.x,
+				z - rhs.z
+		);
+	}
+
+	Vector2 operator-() const {
+		return Vector2<T> (
+				-x,
+				-z
+		);
+	}
+
+	Vector2 operator*(const Vector2 &rhs) const {
+		return Vector2<T>(
+				x * rhs.x,
+				z * rhs.z
+		);
+	}
+
+	Vector2 operator/(const Vector2 &rhs) const {
+		return Vector2<T>(
+				x / rhs.x,
+				z / rhs.z
+		);
+	}
+
+	bool operator==(const Vector2 &rhs) const {
+		return (x == rhs.x && z == rhs.z);
+	}
+
+	bool operator!=(const Vector2 &rhs) const {
+		return !(*this == rhs);
+	}
+
+	bool operator<(const Vector2 &rhs) const {
+		if (x < rhs.x)
+			return true;
+		if (rhs.x < x)
+			return false;
+		return z < rhs.z;
+	}
+
+
+	friend std::ostream &operator<<(std::ostream &os, const Vector2 &vector2) {
+		os << vector2.x << " " << vector2.y << " " << vector2.z;
+		return os;
+	}
+};
+
 using VectorF = Vector3<double>;
 using Vector = Vector3<signed long long>;
+
+using Vector2I = Vector2<int32_t>;
